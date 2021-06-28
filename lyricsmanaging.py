4a33ecf7558f4e26cc648388216e80a89b0c4acc
mohanedattef <41030825+mohanedattef@users.noreply.github.com>
2021-06-28 08:41:17 +0200

    Add files via upload

---
+++ lyricsmanaging.py
@@ -0,0 +1,45 @@
+
+#open up the full lyrics file, utf8 encoding because the lyrics are in arabic
+rlyrics= open('lyrics.txt', encoding='utf-8')
+#put them into a list to mange
+lyrics=rlyrics.readlines()
+#figure the number of lines
+lines=len(lyrics)
+#open up the empty lyrics file that we're gonna fill, use w for write permessions 
+lyrics2=open('lyrics2.txt','w', encoding='utf-8')
+#counter
+r=0
+#iterate over the entire file to get a line and the line after it, strip it from the \n then join them together
+#  with a " | "  to help later in the actual lyrics posting
+for lyric in lyrics:
+    if r<lines:
+        x1=lyrics[r]
+        x2=lyrics[r+1]
+        x1=x1.strip()
+        x2=x2.strip()
+        if x1=='':
+            r=r+1
+            continue
+        elif x2=='':
+            lyrics2.write(x1)
+            lyrics2.write('\n')
+            r=r+2
+            continue
+        elif x2=='x' :
+            lyrics2.write(x1)
+            lyrics2.write('\n')
+
+            r=r+1
+            continue
+        elif x1=='x':
+       
+            r=r+1
+            continue
+
+        x=''.join(x1+ '|'+x2)
+        lyrics2.write(x)
+        r=r+2
+        lyrics2.write('\n')
+
+#close the file
+lyrics2.close()
